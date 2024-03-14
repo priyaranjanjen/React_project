e92258b77d8f01f48b7370b30bfd22cd3bb88063
Priyaranjan <priyaranjanj8@gmail.com>
2024-03-14 12:52:37 +0530

    used useEffect hook to render the random images on page load

--- src/dogs.jsx
+++ src/dogs.jsx
@@ -1,28 +1,50 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './dogs.css';
 function FetchDog(){
 
-    const [selectBreed, setSelectBreed] = useState()
     const [breedImage, setBreedImage] = useState([]);
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
     const fetchApi = async(e) =>{
 
-        if(e.target.value === "random"){
-            let rawApiData = await fetch("https://dog.ceo/api/breeds/image/random");
-            let finalApiData = await rawApiData.json();
-            setBreedImage(finalApiData.message);
-            console.log(finalApiData.message);
+        let finalApiData;
+        setCurrentImageIndex(0);
+
+        try {
+            if(e.target.value === "random"){
+                let rawApiData = await fetch("https://dog.ceo/api/breeds/image/random");
+                finalApiData = await rawApiData.json();
+
+                console.log(finalApiData);
+
+                setBreedImage([finalApiData.message]);
+            }else{
+                let rawApiData = await fetch(`https://dog.ceo/api/breed/${e.target.value}/images`)
+                finalApiData = await rawApiData.json();
+                
+                setBreedImage(finalApiData.message)
+            }
+
+            console.log(breedImage);
+        } catch (error) {
+            alert("Error in fetching Data");
+        }
+    }
+
+    useEffect(() => {
+        const randomPic = async() => {
+            try {
+                const rawApiData = await fetch("https://dog.ceo/api/breeds/image/random");
+                const finalApiData = await rawApiData.json();
+    
+                setBreedImage([finalApiData.message]);
+            } catch (error) {
+                console.log("error!")
+            }
         }
 
-        let rawApiData = await fetch(`https://dog.ceo/api/breed/${e.target.value}/images`)
-        let finalApiData = await rawApiData.json();
-        // console.log(finalApiData);  
-        // return finalApiData;
-
-        setBreedImage(finalApiData.message)
-        // console.log(breedImage);
-    }
+        randomPic();
+    }, [])
 
     const nextImage = () => {
         setCurrentImageIndex((prevIndex) => prevIndex+1);
@@ -39,7 +61,7 @@ function FetchDog(){
                 <option value="husky">Husky</option>
             </select>
             <div>
-                <img src={breedImage[currentImageIndex]} alt={selectBreed}></img>
+                <img src={breedImage[currentImageIndex]} alt={currentImageIndex}></img>
             </div>
             <div>
                 <button onClick={nextImage}>Next</button>
@@ -49,3 +71,5 @@ function FetchDog(){
 }
 
 export default FetchDog;
+
+// http://dog.ceo/api/breeds/list/all
